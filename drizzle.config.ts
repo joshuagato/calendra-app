13288e3c3d466c3f8af5cb80a741f62769c3061f
Joshua Gato <joshuagato37@gmail.com>
2025-11-28 18:03:20 +0000

    Added drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,26 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { defineConfig } from "drizzle-kit";
+
+
+const databaseUrl = process.env.DATABASE_URL;
+
+
+if (!databaseUrl) {
+    throw new Error("DATABASE_URL is not defined in environment varibales");
+}
+
+export default defineConfig({
+    schema: "./drizzle/schema.ts",
+
+    out: "./drizzle/migrations",
+
+    dialect: "postgresql",
+
+    strict: true,
+
+    verbose: true,
+
+    dbCredentials: {
+        url: databaseUrl!
+    }
+})
